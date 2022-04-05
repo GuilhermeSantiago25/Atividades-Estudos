3d579429ea76b1262962502ea5d6138b50247d8c
Guilherme Santiago de Oliveira <guisanti.oliveira@gmail.com>
2022-04-05 09:00:07 -0300

    Fazendo alterações no js

--- introducao-javascript/js/imc.js
+++ introducao-javascript/js/imc.js
@@ -1,54 +1,26 @@
-// Calc IMC Paciente 1;
-let pesoPaulo = document.querySelector('#idPesoPaulo');
-let alturaPaulo = document.querySelector('#idAlturaPaulo');
+let pacientes = document.querySelectorAll('.paciente')
+for (let i = 0; i < pacientes.length; i++) {
+    let paciente = pacientes[i]
+    let tdPeso = paciente.querySelector ('.info-peso').textContent;
+    let tdAltura = paciente.querySelector('.info-altura').textContent;
+    let tdimc = paciente.querySelector('.info-imc').textContent;
 
-let tdPesoPaulo = pesoPaulo.textContent;
-let tdAlturaPaulo = alturaPaulo.textContent;
+    let pesoValido = true;
+    let alturaValida = true;
 
-let imcPaulo = tdPesoPaulo / (tdAlturaPaulo * tdAlturaPaulo);
-let tdImcPaulo = document.querySelector('#idImcPaulo');
-tdImcPaulo.textContent = imcPaulo.toFixed(2);
-
-// Calc IMC Paciente 2;
-let pesoJoao = document.querySelector('#idPesoJoao');
-let alturaJoao = document.querySelector('#idAlturaJoao');
-
-let tdPesoJoao = pesoJoao.textContent;
-let tdAlturaJoao = alturaJoao.textContent;
-
-let imcJoao = tdPesoJoao / (tdAlturaJoao * tdAlturaJoao);
-let tdImcJoao = document.querySelector('#idImcJoao');
-tdImcJoao.textContent = imcJoao.toFixed(2);
-
-// Calc IMC Paciente 3;
-let pesoErica = document.querySelector('#idPesoErica');
-let alturaErica = document.querySelector('#idAlturaErica');
-
-let tdPesoErica = pesoErica.textContent;
-let tdAlturaErica = alturaErica.textContent;
-
-let imcErica = tdPesoErica / (tdAlturaErica * tdAlturaErica);
-let tdImcErica = document.querySelector('#idImcErica');
-tdImcErica.textContent = imcErica.toFixed(2);
-
-// Calc IMC Paciente 4;
-let pesoDouglas = document.querySelector('#idPesoDouglas');
-let alturaDouglas = document.querySelector('#idAlturaDouglas');
-
-let tdPesoDouglas = pesoDouglas.textContent;
-let tdAlturaDouglas = alturaDouglas.textContent;
-
-let imcDouglas = tdPesoDouglas / (tdAlturaDouglas * tdAlturaDouglas);
-let tdImcDouglas = document.querySelector('#idImcDouglas');
-tdImcDouglas.textContent = imcDouglas.toFixed(2);
-
-// Calc IMC Paciente 5;
-let pesoTatiana = document.querySelector('#idPesoTatiana');
-let alturaTatiana = document.querySelector('#idAlturaTatiana');
-
-let tdPesoTatiana = pesoTatiana.textContent;
-let tdAlturaTatiana = alturaTatiana.textContent;
-
-let imcTatiana = tdPesoTatiana / (tdAlturaTatiana * tdAlturaTatiana);
-let tdImcTatiana = document.querySelector('#idImcTatiana');
-tdImcTatiana.textContent = imcTatiana.toFixed(2);
+    if (tdPeso <=0 || tdPeso >= 1000){
+        console.log('Peso inválido')
+        tdimc.textContent = 'Peso Inválido!';
+        pesoValido = false;
+    } 
+    if (tdAltura <=0 || tdAltura >= 1000){
+        console.log('Altura inválida')
+        tdimc.textContent = 'Altura Inválida!';
+        alturaValida = false;
+    } 
+    if (tdPeso && tdAltura){
+        let imc = tdPeso / (tdAltura * tdAltura);
+        tdimc.textContent = imc;
+        
+    } 
+}
