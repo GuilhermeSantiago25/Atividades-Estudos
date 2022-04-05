5e920b9501b00e31b2dc9cfc28bfaf9202c3b10e
Guilherme Santiago de Oliveira <guisanti.oliveira@gmail.com>
2022-04-05 09:03:21 -0300

    Corrigindo erro de calculo do imc

--- introducao-javascript/js/imc.js
+++ introducao-javascript/js/imc.js
@@ -1,26 +1,29 @@
 let pacientes = document.querySelectorAll('.paciente')
 for (let i = 0; i < pacientes.length; i++) {
     let paciente = pacientes[i]
-    let tdPeso = paciente.querySelector ('.info-peso').textContent;
-    let tdAltura = paciente.querySelector('.info-altura').textContent;
-    let tdimc = paciente.querySelector('.info-imc').textContent;
+    let tdPeso = paciente.querySelector ('.info-peso');
+    let tdAltura = paciente.querySelector('.info-altura');
+    let tdimc = paciente.querySelector('.info-imc');
+    
+    let peso = tdPeso.textContent;
+    let altura = tdAltura.textContent;
 
     let pesoValido = true;
     let alturaValida = true;
 
-    if (tdPeso <=0 || tdPeso >= 1000){
+    if (peso <=0 || peso >= 1000){
         console.log('Peso inválido')
         tdimc.textContent = 'Peso Inválido!';
         pesoValido = false;
     } 
-    if (tdAltura <=0 || tdAltura >= 1000){
+    if (altura <=0 || altura >= 1000){
         console.log('Altura inválida')
         tdimc.textContent = 'Altura Inválida!';
         alturaValida = false;
     } 
-    if (tdPeso && tdAltura){
-        let imc = tdPeso / (tdAltura * tdAltura);
-        tdimc.textContent = imc;
+    if (peso && altura){
+        let imc = peso / (altura * altura);
+        tdimc.textContent = imc.toFixed(2);
         
     } 
 }
